39ff1ce88851fc85799b6a9b5ebad98a58d0b50c
flandre8233 <flandre8233@gmail.com>
2017-05-25 04:37:36 +0800

    Add files via upload

---
+++ ConVol/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ConVol
+{
+    partial class VolControl
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(311, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 16);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "open double speed";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(368, 52);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(54, 16);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "setting";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Enabled = false;
+            this.radioButton1.Location = new System.Drawing.Point(198, 52);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(86, 16);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "set Vol Down";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Enabled = false;
+            this.radioButton2.Location = new System.Drawing.Point(290, 52);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(72, 16);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "set Vol Up";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // VolControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 71);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.Name = "VolControl";
+            this.Text = "VolControl";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.VolControl_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ ConVol/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using myFunction;
+
+namespace ConVol
+{
+    public partial class VolControl : Form
+    {
+        const int Mute = 0x80000; // APPCOMMAND_VOLUME_MUTE
+        const int Up = 0xA0000;
+        const int Down = 0x90000;
+        const int WM_APPCOMMAND = 0x319;
+        
+        Function myfunction= new Function();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hwnd, int Msg, IntPtr wParam, IntPtr lparam);
+
+        //global hotkey
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hwnd, int id, int fsModifiers, int vlc);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hwnd, int id);
+
+        const int MYACTION_HOTKEY_UP_VolUP = 1;
+        const int MYACTION_HOTKEY_Down_VolDown = 2;
+        const int MYACTION_HOTKEY_Left_VolDown = 3;
+        const int MYACTION_HOTKEY_Right_VolUp = 4;
+        const int MYACTION_HOTKEY_ShowAndHide = 5;
+
+        const int MYACTION_HOTKEY_Snake = 6;
+    
+        bool IsHide;
+        bool OpenDoubleSpeed;
+        public VolControl()
+        {
+            // Modifier Keys codes:Alt = 1,Ctrl = 2,Shift = 4,Win = 8
+            //Compute the addition of each combination of the keys you want to be pressed
+            //ALT+CTRL = 1+2=3,CTRL+SHIFT = 2+4=6...
+            //RegisterHotKey(this.Handle, MYACTION_HOTKEY_UP_VolUP, 1, (int)Keys.Up);
+            //RegisterHotKey(this.Handle, MYACTION_HOTKEY_Down_VolDown, 1, (int)Keys.Down);
+            
+            if (Properties.Settings.Default.VolDown == 0 && Properties.Settings.Default.VolUp == 0) {
+                RegisterHotKey(this.Handle, MYACTION_HOTKEY_Left_VolDown, 1, (int)Keys.OemOpenBrackets);
+                RegisterHotKey(this.Handle, MYACTION_HOTKEY_Right_VolUp, 1, (int)Keys.OemCloseBrackets);
+            }
+            else {
+                RegisterHotKey(this.Handle, MYACTION_HOTKEY_Left_VolDown, 1, Properties.Settings.Default.VolDown);
+                RegisterHotKey(this.Handle, MYACTION_HOTKEY_Right_VolUp, 1, Properties.Settings.Default.VolUp);
+            }
+            
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ShowAndHide, 1, (int)Keys.X);
+
+            //RegisterHotKey(this.Handle, MYACTION_HOTKEY_Snake, 1, (int)Keys.Z);
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            label1.Text = "ALT + X to Show And Hide ";
+
+            if (Properties.Settings.Default.VolDown == 0 && Properties.Settings.Default.VolUp == 0) {
+                label2.Text = "ALT + [ VOL-          ALT + ] VOL+";
+            }
+            else {
+                label2.Text = "ALT + "+  myfunction.keysStringConvert(((Keys)Properties.Settings.Default.VolDown).ToString()) + " VOL-          ALT +  " + myfunction.keysStringConvert(((Keys)Properties.Settings.Default.VolUp).ToString()) + "  VOL+";
+            }
+                
+
+            
+
+        }
+            //SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)Mute);
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            
+            switch (m.Msg)
+            {
+                case 0x0200:
+                    break;
+                case 0x312:
+                    switch (m.WParam.ToInt32())
+                    {
+                        case MYACTION_HOTKEY_Snake:
+                            break;
+
+                        case MYACTION_HOTKEY_UP_VolUP:
+
+                            break;
+                        case MYACTION_HOTKEY_Down_VolDown:
+                            
+                            break;
+                        case MYACTION_HOTKEY_Left_VolDown:
+                            if (OpenDoubleSpeed == false)
+                            {
+                                SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)Down);
+                            }
+                            else
+                            {
+                                SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)Down);
+                                SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)Down);
+                            }
+                            
+                            break;
+                        case MYACTION_HOTKEY_Right_VolUp:
+                            if (OpenDoubleSpeed == false)
+                            {
+                                SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)Up);
+                            }
+                            else
+                            {
+                                SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)Up);
+                                SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)Up);
+                            }
+                            
+                            break;
+                        case MYACTION_HOTKEY_ShowAndHide:
+                            if (IsHide == true)
+                            {
+                                Show();
+                                IsHide = false;
+                            }
+                            else
+                            {
+                                Hide();
+                                IsHide = true;
+                            }
+                            break;
+                    }
+                    break;
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_UP_VolUP);
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_Down_VolDown);
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_Left_VolDown);
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_Right_VolUp);
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_ShowAndHide);
+            Properties.Settings.Default.Save();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+            {
+                OpenDoubleSpeed = false;
+            }
+            else
+            {
+                OpenDoubleSpeed = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) {
+            if (checkBox2.Checked) {
+                radioButton1.Enabled = true;
+                radioButton2.Enabled = true;
+            }
+            else {
+                radioButton1.Enabled = false;
+                radioButton2.Enabled = false;
+            }
+        }
+
+        private void VolControl_KeyUp(object sender, KeyEventArgs e) {
+
+            if (checkBox2.Checked) {
+                if (radioButton1.Checked) {
+                    Properties.Settings.Default.VolDown = e.KeyValue;
+                    MessageBox.Show("VolDown 已更改為" + myfunction.keysStringConvert(((Keys)Properties.Settings.Default.VolDown).ToString()) + "下次開啟就有效");
+                }
+                else {
+                    Properties.Settings.Default.VolUp = e.KeyValue;
+                    MessageBox.Show("VolUp 已更改為" + myfunction.keysStringConvert(((Keys)Properties.Settings.Default.VolUp).ToString()) + "下次開啟就有效");
+                }
+            }
+
+        }
+    }
+}
